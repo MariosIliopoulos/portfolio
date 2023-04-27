03f053c271d16246495da48cf13afa5a05b74c87
Marios <mariosiliop7@gmail.com>
2023-04-27 15:07:06 +0300

    update responsive

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,52 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./sidebar.css";
 
 
 const Sidebar = () => {
+
+  const [toggle, showMenu] = useState(false);
+
   return (
-    <aside className='aside'>
+    <>
+      <aside className={toggle ? "aside show-menu" : "aside"}>
 
-      <nav className="nav">
-        <div className="nav__menu">
-          <ul className="nav__list">
-            <li className="nav__item">
-              <a href="#home" className="nav__link">
-                <i className="icon-home"></i>
-              </a>
-            </li>
+        <nav className="nav">
+          <div className="nav__menu">
+            <ul className="nav__list">
+              <li className="nav__item">
+                <a href="#home" className="nav__link">
+                  <i className="icon-home"></i>
+                </a>
+              </li>
 
-            <li className="nav__item">
-              <a href="#about" className="nav__link">
-                <i className="icon-user"></i>
-              </a>
-            </li>
+              <li className="nav__item">
+                <a href="#about" className="nav__link">
+                  <i className="icon-user"></i>
+                </a>
+              </li>
 
-            <li className="nav__item">
-              <a href="#resume" className="nav__link">
-                <i className="icon-graduation"></i>
-              </a>
-            </li>
+              <li className="nav__item">
+                <a href="#resume" className="nav__link">
+                  <i className="icon-graduation"></i>
+                </a>
+              </li>
 
-            <li className="nav__item">
-              <a href="#work" className="nav__link">
-                <i className="icon-layers"></i>
-              </a>
-            </li>
+              <li className="nav__item">
+                <a href="#work" className="nav__link">
+                  <i className="icon-layers"></i>
+                </a>
+              </li>
 
-            <li className="nav__item">
-              <a href="#contact" className="nav__link">
-                <i className="icon-bubble"></i>
-              </a>
-            </li>
-          </ul>
+              <li className="nav__item">
+                <a href="#contact" className="nav__link">
+                  <i className="icon-bubble"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <div className="nav__footer">
+          <span className="copyright">&copy; Iliopoulos</span>
         </div>
-      </nav>
 
-      <div className="nav__footer">
-        <span className="copyright">&copy; Iliopoulos</span>
+      </aside>
+
+      <div className={toggle ? "nav__toggle nav__toggle-open" : "nav__toggle"} onClick={() => showMenu(!toggle)}>
+        <i className="icon-menu"></i>
       </div>
 
-    </aside>
+    </>
   )
 }
 
