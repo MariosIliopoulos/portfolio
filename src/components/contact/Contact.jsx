ca0a4d204bf9be2679f349b7e143af7776ce9481
Marios <mariosiliop7@gmail.com>
2023-04-27 11:27:15 +0300

    update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -7,30 +7,24 @@ const Contact = () => {
       <h2 className="section__title">Contact</h2>
 
       <div className="contact__container grid">
-        <div className="contact__info">
-          <h3 className="contact__title">Fill the form</h3>
-        </div>
 
-        <form className="contact__form" method="POST">
+        <div className="contact__form">
           <div className="contact__form-group">
+            <h3 className="contact__title">Name:</h3>
             <div className="contact__form-div">
-              <input type="text" className="contact__form-input" name='name' placeholder='Insert your name'/>
+              <p className='contact__form-desc'>Iliopoulos Marios</p>
             </div>
-
+            <h3 className="contact__title">Email:</h3>
             <div className="contact__form-div">
-              <input type="email" className="contact__form-input" name='email' placeholder='Insert your email'/>
+              <p className='contact__form-desc'>mariosiliop7@gmail.com</p>
+            </div>
+            <h3 className="contact__title">Phone number:</h3>
+            <div className="contact__form-div">
+              <p className='contact__form-desc'>+306980753533</p>
             </div>
           </div>
-          <div className="contact__form-div">
-            <input type="text" className="contact__form-input" name='subject' placeholder='Insert your subject'/>
-          </div>
-
-          <div className="contact__form-div contact__form-area">
-            <textarea name="message" id="" cols="30" rows="10" className='contact__form-input' placeholder='Write your message'></textarea>
-          </div>
-
-          <button className='btn' type='submit' value='SEND'>Send message</button>
-        </form>
+          
+        </div>
       </div>
     </section>
   )
